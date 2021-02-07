ce35dcd12012cb6a65287dd408f3e8e6e966599f
wtwerner <wtwerner@gmail.com>
2021-02-07 14:34:02 -0600

    Namespace games routes

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,4 +1,4 @@
-class GamesController < ApplicationController
+class Api::V1::GamesController < ApplicationController
   before_action :set_game, only: [:show, :update, :destroy]
 
   # GET /games
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :games
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :games
     end
   end
 end
