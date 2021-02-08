a306f740285a3827c42be1d814920c4d4f15b205
wtwerner <wtwerner@gmail.com>
2021-02-07 21:39:19 -0600

    Add logged_in helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,8 @@ class ApplicationController < ActionController::API
             # mocked login
             User.first
         end
+        def logged_in?
+            !!current_user
+        end
     end
 end
