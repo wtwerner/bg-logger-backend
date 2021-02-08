f222c61da4eb27951261200a39048abfd1e4e8ea
wtwerner <wtwerner@gmail.com>
2021-02-07 21:25:26 -0600

    Create user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,12 +5,14 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    users_json = UserSerializer.new(@users).serialized_json
+    render json: users_json
   end
 
   # GET /users/1
   def show
-    render json: @user
+    user_json = UserSerializer.new(@user).serialized_json
+    render json: user_json
   end
 
   # POST /users
