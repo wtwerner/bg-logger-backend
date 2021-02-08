69ed17906d4e6d8943816c648d3543856072aa5b
wtwerner <wtwerner@gmail.com>
2021-02-08 09:15:41 -0600

    Remove mock user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::API
-    helpers do
-        def current_user
-            # mocked login
-            User.first
-        end
-        def logged_in?
-            !!current_user
-        end
-    end
+
 end
