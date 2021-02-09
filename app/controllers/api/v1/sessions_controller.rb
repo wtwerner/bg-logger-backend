c216a1f5d88dd73df5f74146903621d6379a880f
wtwerner <wtwerner@gmail.com>
2021-02-09 12:12:47 -0600

    Finish login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,33 +1,18 @@
 class Api::V1::SessionsController < ApplicationController
-    
     def create
-        @user = User.find_by(username: params[:session][:username])
-  
-        if @user && @user.authenticate(params[:session][:password])
+        @user = User.find_by(email: params[:email])
+        
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
-            render json: UserSerializer.new(@user), status: :ok
+            render json: @user
         else
             render json: {
-            error: "Invalid Credentials"
-        }
-        end
-    end
-  
-    def get_current_user
-        if logged_in?
-            render json: UserSerializer.new(current_user)
-        else
-            render json: {
-                error: "No one logged in"
+                error: "Invalid Credentials"
             }
         end
     end
-    
+
     def destroy
-        session.clear
-        render json: {
-            notice: "successfully logged out"
-        }, status: :ok
+
     end
-  
 end
