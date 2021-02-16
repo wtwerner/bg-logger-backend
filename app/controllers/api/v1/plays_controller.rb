966205900bd06eac2d37b5ce1493df9cf2cbc7f9
wtwerner <wtwerner@gmail.com>
2021-02-16 12:29:22 -0600

    Create plays controller

---
+++ app/controllers/api/v1/plays_controller.rb
@@ -0,0 +1,63 @@
+class Api::V1::PlaysController < ApplicationController
+    before_action :set_play, only: [:show, :update, :destroy]
+
+    # GET /games
+    def index
+      @plays = Play.all
+  
+      render json: @plays
+    end
+  
+    # GET /games/1
+    def show
+      render json: @play
+    end
+  
+    # POST /games
+    def create
+        @play = Play.new(
+          user_id: current_user.id, 
+          game_id: params[:game_id], 
+          date: params[:date], 
+          players: params[:players], 
+          duration: params[:duration], 
+          winner: params[:winner], 
+          notes: params[:notes]
+        )
+  
+      if @play.save
+        render json: @play
+      else
+        render json: @play.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /games/1
+    def update
+      if @play.update(play_params)
+        render json: @play
+      else
+        render json: @play.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /games/1
+    def destroy
+      if Play.destroy(@play.id)
+        render json: @play
+      else
+        render json: @play.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+    def set_play
+    @play = Play.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def play_params
+    params.require(:play).permit(:id, :game_id, :user_id, :date, :winner, :notes, :duration, :players)
+    end
+end
