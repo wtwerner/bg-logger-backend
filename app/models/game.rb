10eb938c26ad9f21a505c0d561eecf297f616d4c
wtwerner <wtwerner@gmail.com>
2021-02-21 19:19:24 -0600

    Move validations to proper model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,4 @@
 class Game < ApplicationRecord
     belongs_to :user
     has_many :plays
-
-    validates :bga_id, presence: true
-    validates :date, presence: true
 end
--- app/models/play.rb
+++ app/models/play.rb
@@ -1,3 +1,6 @@
 class Play < ApplicationRecord
     belongs_to :game
+
+    validates :bga_id, presence: true
+    validates :date, presence: true
 end
