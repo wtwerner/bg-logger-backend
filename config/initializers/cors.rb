7eabd9ea104f27f1b97e2a16de9527d7fe9f8612
wtwerner <wtwerner@gmail.com>
2021-03-02 11:09:15 -0600

    Adjust cors back to localhost origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bg-logger-frontend.herokuapp.com'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
