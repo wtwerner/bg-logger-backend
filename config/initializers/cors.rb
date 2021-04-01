ef8ded2a1ab77b137bc8d597bd79ddf0c2c98089
wtwerner <wtwerner@gmail.com>
2021-04-01 14:10:57 -0500

    Adjust cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'https://bg-logger-frontend.herokuapp.com/'
+    origins ['http://localhost:3000', 'http://localhost:3001', 'https://bg-logger-frontend.herokuapp.com']
 
     resource '*',
       headers: :any,
